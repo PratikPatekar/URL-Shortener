7bed061b02200e99173df87e47ff06c7d311190d
Pratik Patekar <pratik.patekar3@gmail.com>
2020-07-17 12:43:24 +0530

    Updated Readme.md

--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    #secret key required for POST method
+    #secret key required for POST method (I have used some random gibberish)
     app.secret_key = 'adjasnlnacwn2ne2queh398rn3f2e015'
     app.config['MONGO_URI'] = "mongodb://localhost:27017/Users"
     mongo = PyMongo(app)
